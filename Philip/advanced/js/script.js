2939631a48160cef600ce441ce9f6fa034079b13
philipm92 <philip.mahlberg@hotmail.com>
2021-07-07 15:37:05 +0200

    cheated or "trick 17" second row

--- Philip/advanced/js/script.js
+++ Philip/advanced/js/script.js
@@ -1,5 +1,5 @@
 // console.log("Hello World");
-var menu_headlines = ["Starters"];
+var menu_headlines = ["Starters", "Main Courses"];
 // class FoodItems {
 //   name: string;
 //   price: number;
@@ -23,18 +23,41 @@ var menu_items = [
     { name: "Pumpkin Soup (vegan)", price: 3.99, img: "img/starter2_pumpkinsoup_new.jpg", desc: "Pumpkin soup made with coconut milk & carrots." },
     { name: "Smoothie (vegan)", price: 3.49, img: "img/starter3_smoothie_new.jpg", desc: "Made with avocado, seasonal fruits, flaxseeds & almond milk." },
     { name: "Sushi", price: 11.9, img: "img/starter4_sushi_new.jpg", desc: "6 pieces of maki & 5 pieces of sushi." },
+    { name: "Rib-Eye Steak <small>organic</small>", price: 12.99, img: "img/main1_steak_new.jpg", desc: "One pound organic rib-eye steak fried in ghee." },
+    { name: "Pizza Cardinale &Oslash; 33cm", price: 8.45, img: "img/main2_pizzaham_new.jpg", desc: "Pizza with ham, olives, bell peppers & mushrooms." },
+    { name: "Smoothie (vegan)Salmon with Veggies", price: 5.95, img: "img/main3_salmon_new.jpg", desc: "One salmon fillet served with mixed-veggies &amp; lemon slices." },
+    { name: "Pasta alla Bolognese <small>(meat/vegan)</small>", price: 4.29, img: "img/main4_spaghetti_new.jpg", desc: "Spaghetti or penne with meat sauce or tempeh sauce." },
 ];
 var section_element = document.getElementById("menusection");
 var html_string = "";
+var index = 0;
+var n = 3;
 for (var _i = 0, menu_headlines_1 = menu_headlines; _i < menu_headlines_1.length; _i++) {
     var headline = menu_headlines_1[_i];
     html_string += "<h2 class=\"text-center mt-3 mt-md-4 mb-2 mb-mb-3\">" + headline + "</h2>"; // Create sub-headline
     html_string += "<div class=\"row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 g-xxl-5 px-2 px-md-4 pb-4 mx-auto\">"; // Start "Review" Grid 
     // Create Food Card
-    for (var _a = 0, menu_items_1 = menu_items; _a < menu_items_1.length; _a++) {
-        var food = menu_items_1[_a];
+    // for (let food of menu_items) {
+    //   html_string += `
+    //     <div class="col col-md-6 col-lg-3">                
+    //       <div class="card h-100 imgzoom">
+    //           <img src="${food.img}" class="img-fluid rounded-start" alt="${food.name}">
+    //           <div class="card-body">
+    //               <h5 class="card-title">${food.name}</h5>
+    //               <p class="card-text">${food.desc}</p>
+    //               <p class="card-text fw-bold">&euro;${food.price}</p>
+    //           </div>
+    //       </div>
+    //     </div>      
+    //   `;
+    // }
+    console.log(index, n);
+    for (index; index <= n; index++) {
+        var food = menu_items[index];
         html_string += "\n      <div class=\"col col-md-6 col-lg-3\">                \n        <div class=\"card h-100 imgzoom\">\n            <img src=\"" + food.img + "\" class=\"img-fluid rounded-start\" alt=\"" + food.name + "\">\n            <div class=\"card-body\">\n                <h5 class=\"card-title\">" + food.name + "</h5>\n                <p class=\"card-text\">" + food.desc + "</p>\n                <p class=\"card-text fw-bold\">&euro;" + food.price + "</p>\n            </div>\n        </div>\n      </div>      \n    ";
     }
+    index = 4;
+    n = 7;
     // End "Review" Grid
     html_string += "</div>";
 }
--- Philip/advanced/js/script.ts
+++ Philip/advanced/js/script.ts
@@ -1,6 +1,6 @@
 // console.log("Hello World");
 
-const menu_headlines: string[] = ["Starters"];
+const menu_headlines: string[] = ["Starters", "Main Courses"];
 
 // class FoodItems {
 //   name: string;
@@ -28,16 +28,41 @@ let menu_items: Array<{ name: string, price: number, img: string, desc?: string}
   { name: "Pumpkin Soup (vegan)", price: 3.99, img: "img/starter2_pumpkinsoup_new.jpg", desc: "Pumpkin soup made with coconut milk & carrots." },
   { name: "Smoothie (vegan)", price: 3.49, img: "img/starter3_smoothie_new.jpg", desc: "Made with avocado, seasonal fruits, flaxseeds & almond milk." },
   { name: "Sushi", price: 11.9, img: "img/starter4_sushi_new.jpg", desc: "6 pieces of maki & 5 pieces of sushi." },
+
+  { name: "Rib-Eye Steak <small>organic</small>", price: 12.99, img: "img/main1_steak_new.jpg", desc: "One pound organic rib-eye steak fried in ghee." },
+  { name: "Pizza Cardinale &Oslash; 33cm", price: 8.45, img: "img/main2_pizzaham_new.jpg", desc: "Pizza with ham, olives, bell peppers & mushrooms." },
+  { name: "Smoothie (vegan)Salmon with Veggies", price: 5.95, img: "img/main3_salmon_new.jpg", desc: "One salmon fillet served with mixed-veggies &amp; lemon slices." },
+  { name: "Pasta alla Bolognese <small>(meat/vegan)</small>", price: 4.29, img: "img/main4_spaghetti_new.jpg", desc: "Spaghetti or penne with meat sauce or tempeh sauce." },
+
 ];
 
 var section_element = document.getElementById("menusection");
 var html_string = "";
+var index: number = 0;
+var n: number = 3;
 for (let headline of menu_headlines) {
   html_string += `<h2 class="text-center mt-3 mt-md-4 mb-2 mb-mb-3">${headline}</h2>`; // Create sub-headline
   html_string += "<div class=\"row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 g-xxl-5 px-2 px-md-4 pb-4 mx-auto\">"; // Start "Review" Grid 
 
   // Create Food Card
-  for (let food of menu_items) {
+  // for (let food of menu_items) {
+  //   html_string += `
+  //     <div class="col col-md-6 col-lg-3">                
+  //       <div class="card h-100 imgzoom">
+  //           <img src="${food.img}" class="img-fluid rounded-start" alt="${food.name}">
+  //           <div class="card-body">
+  //               <h5 class="card-title">${food.name}</h5>
+  //               <p class="card-text">${food.desc}</p>
+  //               <p class="card-text fw-bold">&euro;${food.price}</p>
+  //           </div>
+  //       </div>
+  //     </div>      
+  //   `;
+  // }
+
+  console.log(index, n);
+  for (index; index <= n; index++) {
+    let food: any = menu_items[index];
     html_string += `
       <div class="col col-md-6 col-lg-3">                
         <div class="card h-100 imgzoom">
@@ -51,6 +76,8 @@ for (let headline of menu_headlines) {
       </div>      
     `;
   }
+  index = 4;
+  n = 7;
 
   // End "Review" Grid
   html_string += "</div>";  
